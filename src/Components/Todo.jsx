85a84931dda8cb94611945663330e2bd1b45ac7a
maxlestage <maxlestage@icloud.com>
2022-01-23 20:15:03 +0100

    Finish 🥳
    - Quand l'utilisateur clique sur un élément de la liste, l'élément doit être marqué comme "fait" en rayant de la liste ( utilisez la propriété CSS 'text-décoration: 'line-through' ).
    
    - Quand l'utilisateur clique à nouveau sur un élément rayé le status "fait" est annulé ( l'élément n'est plus rayé ).

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 let textId = 0;
 
+// <li style={{ textDecoration: "line-trough" }}>item 0</li>
+//     <li style={{ textDecoration: "none" }}>item 1</li>
+
 function Todo() {
   const [myText, setMyText] = useState("");
   const [savedTexts, setSavedTexts] = useState([]);
@@ -13,17 +16,42 @@ function Todo() {
       <button
         onClick={() => {
           setMyText("");
-          setSavedTexts([{ id: textId++, text: myText }, ...savedTexts]);
+          setSavedTexts([
+            { id: textId++, text: myText, todo: false },
+            ...savedTexts,
+          ]);
         }}
       >
         Add
       </button>
       <ul>
-        {/* <li style={{ textDecoration: "line-trough" }}>item 0</li>
-        <li style={{ textDecoration: "none" }}>item 1</li>
-        <li>...</li> */}
-        {savedTexts.map((text) => (
-          <li key={text.id}>{text.text}</li>
+        {savedTexts.map((txt, index) => (
+          <li
+            key={index}
+            style={
+              txt.todo === false
+                ? { textDecoration: "none" }
+                : { textDecoration: "line-through" }
+            }
+            onClick={() => {
+              console.log("Premier", [...savedTexts]);
+
+              setSavedTexts(
+                [
+                  ...savedTexts.slice(0, index),
+                  txt.todo === false
+                    ? { id: txt.id, text: txt.text, todo: true }
+                    : { id: txt.id, text: txt.text, todo: false },
+                  ...savedTexts.slice(index + 1),
+                ]
+
+                // savedTexts.push({ id: text.id, text: text.text, todo: true }),
+                // ...savedTexts,
+              );
+            }}
+          >
+            {txt.text}
+          </li>
         ))}
       </ul>
     </>
